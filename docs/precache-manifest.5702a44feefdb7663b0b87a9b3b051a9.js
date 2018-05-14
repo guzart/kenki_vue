1be3462b0d6e03bde6e624b2be3dc342036c9482
Arturo Guzman <arturo@guzart.com>
2018-05-13 23:14:31 -0400

    fix base url for github docs

---
+++ docs/precache-manifest.5702a44feefdb7663b0b87a9b3b051a9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c74938b1b591a0d4dd2a",
+    "url": "/kenki_vue/js/vendors~app.c74938b1.js"
+  },
+  {
+    "revision": "f5dd0838a6541f98ebb8",
+    "url": "/kenki_vue/js/app.f5dd0838.js"
+  },
+  {
+    "revision": "2f917520cd91dae13a2925cf4810181f",
+    "url": "/kenki_vue/index.html"
+  },
+  {
+    "revision": "f5dd0838a6541f98ebb8",
+    "url": "/kenki_vue/css/app.e1080b16.css"
+  }
+];
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  baseUrl: '/kenki_vue/',
+};
